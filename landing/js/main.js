728be9846e38c4d3f7e058a7bf60b846a35702f9
Aaron Peña <aaronpena104@gmail.com>
2023-08-25 13:20:09 -0700

    primer commit
    Se añade el index, imagenes, css y el js.
    Son la base del proyecto final

---
+++ landing/js/main.js
@@ -0,0 +1,37 @@
+
+console.log('Hola mundo!');
+
+
+/*function mostrarMensajeInicial() {
+
+  if (confirm('Este sitio esta en desarrollo, disculpe las molestias')) {
+    alert('Gracias por visitarnos!');
+  } else {
+    alert('...');
+  }
+}*/
+function goHome(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      scrollToBottomButtom.style.display = 'block';
+    } else {
+      scrollToBottomButtom.style.display = 'none';
+    }
+  })
+
+  scrollToBottomButtom.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  })
+})
